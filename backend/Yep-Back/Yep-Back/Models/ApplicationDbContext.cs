072f7423ffcf4d5e39543edc91d92ae5516c4f0e
PavelVasilchikov <91344984+PavelVasilchikov@users.noreply.github.com>
2023-06-09 20:02:02 +0300

    bd1

---
+++ backend/Yep-Back/Yep-Back/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Yep_Back.Models;
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<User> User { get; set; } = null!;
+
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=LAPTOP-2G2QH5OP;Database=Yep-shop;Trusted_Connection=True;");
+    }
+}
